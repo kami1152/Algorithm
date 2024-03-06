11f70de07315d68203be312cf8bc7cbd9057b221
kami1152 <kami1152@naver.com>
2024-03-06 15:18:38 +0900

    2024-03-06

---
+++ algorithm/data/back_2164.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class back_2164 {
+
+    Deque<Integer> deque = new ArrayDeque<Integer>();
+
+    public back_2164() {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 1; i < N+1; i++) {
+            deque.addLast(i);
+        }
+        while(deque.size() != 1){
+            deque.removeFirst();
+            deque.addLast(deque.pollFirst());
+        }
+        System.out.println(deque.poll());
+    }
+    public static void main(String[] args) {
+        back_2164 a = new back_2164();
+    }
+
+}
