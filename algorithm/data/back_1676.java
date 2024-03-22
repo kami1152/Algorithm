c5164777c3df3037d89d6882a348211fec0bc68a
kami1152 <kami1152@naver.com>
2024-03-22 17:22:01 +0900

    2024-03-22

--- algorithm/data/back_1676.java
+++ algorithm/data/back_1676.java
@@ -3,36 +3,25 @@ package algorithm.data;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.*;
 
 public class back_1676 {
-    static int[] pactorial;
-    static int count = 0;
-
-    public back_1676() throws IOException {
+    static BigInteger bigNumber;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        pactorial = new int[N];
-        pactorial[0] = 1;
-        for (int i = 1; i < N; i++) {
-            pactorial[i] = pactorial[i - 1] * i;
-        }
 
-        for (int i = 0; i < N; i++) {
-            if (String.valueOf(pactorial[i]).contains("0")) {
-                zerocounter(String.valueOf(pactorial[i]));
-            }
-
-        }
-
-        System.out.println(count);
-
-    }
-
-    public static void zerocounter(String str) {
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '0') {
-                count++;
+        int cnt = 0;
+        for(int i = 1; i <= N ; i++){
+            int num = i;
+            while(num %5 ==0){
+                cnt++;
+                num /= 5;
             }
         }
+        System.out.println(cnt);
+        
     }
+
+   
 }
